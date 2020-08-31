cb65c0e12fe62cbe5938ff198e46b423069d5908
Wellington Carlos Massola <wellingtonmassola@gmail.com>
2020-08-31 09:30:35 -0300

    Updates

---
+++ precache-manifest.883707f0d6185f97cbb372c8bf66871f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa112736d86aab919da29374bc9f82dc",
+    "url": "/criador_de_cargas_a_partir_de_xmls/index.html"
+  },
+  {
+    "revision": "d8e79ae6e4f5808e0685",
+    "url": "/criador_de_cargas_a_partir_de_xmls/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "3007eb1531f242770271",
+    "url": "/criador_de_cargas_a_partir_de_xmls/static/css/main.5cfe42db.chunk.css"
+  },
+  {
+    "revision": "d8e79ae6e4f5808e0685",
+    "url": "/criador_de_cargas_a_partir_de_xmls/static/js/2.e570037e.chunk.js"
+  },
+  {
+    "revision": "14d03fbb74f8335c1ef5f011aba90de2",
+    "url": "/criador_de_cargas_a_partir_de_xmls/static/js/2.e570037e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3007eb1531f242770271",
+    "url": "/criador_de_cargas_a_partir_de_xmls/static/js/main.f4ef4344.chunk.js"
+  },
+  {
+    "revision": "deeb6d58ba355352bcb8",
+    "url": "/criador_de_cargas_a_partir_de_xmls/static/js/runtime-main.90715896.js"
+  }
+]);
